1e1de9c6ad4492b10853f58072358ea521963b8e
PixNyb <contact@roelc.me>
2024-06-02 09:54:09 +0000

    Removed the 1 second delay between each reload

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@ const redirect = (res, redirectUrl) => {
   res.status(301).render("redirect", {
     title: "Redirecting...",
     additional_head:
-      "<meta http-equiv='refresh' content='1;url=" + redirectUrl + "'>",
+      "<meta http-equiv='refresh' content='0;url=" + redirectUrl + "'>",
     redirectUrl: redirectUrl,
     show_credit: !FORM_DISABLE_CREDITS,
   });
